e5d9567ba27fadcb2a7debe6e2bda138a49f174f
Douglas Moura <douuuglas@gmail.com>
2025-03-18 10:59:21 -0300

    add: initial project structure

---
+++ Bolado.Browser/Bolado.Browser/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Bolado.Browser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonGo_Click(object sender, RoutedEventArgs e)
+    {
+        if (webView != null && webView.CoreWebView2 != null)
+        {
+            webView.CoreWebView2.Navigate(addressBar.Text);
+        }
+    }
+}
